403fb7fb10764413ec73c7b26748fa206fa0179f
Simon Esders <simonesders2003@gmail.com>
2023-12-12 19:57:31 +0100

    new components

---
+++ src/app/messageinput/messageinput.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messageinput',
+  standalone: true,
+  imports: [],
+  templateUrl: './messageinput.component.html',
+  styleUrl: './messageinput.component.scss'
+})
+export class MessageinputComponent {
+
+}
