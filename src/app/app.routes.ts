6adff8404f8aec84acf4fa601375fbaebb400ffd
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-13 20:22:45 +0100

    header-nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
