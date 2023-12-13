e82af1eea8c0103476be284fb43f1f3f8c8ec83c
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-13 22:03:34 +0100

    test-channel

--- src/app/nav-bar/channels/channels.component.ts
+++ src/app/nav-bar/channels/channels.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './channels.component.scss'
 })
 export class ChannelsComponent {
-
+  changelName:string = 'EntwicklerteamertzuiwegfkuzegfukzgkZGFUZgefkeuzgZEGUZGEZGEZGFZ<GFKZUGSUZ<GFZGS<ZGFZSG<ZGFZUG';
 }
