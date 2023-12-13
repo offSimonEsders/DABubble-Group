a5d4fa282f4bb099dc0b075b3362d4dad060c9bd
Simon Esders <simonesders2003@gmail.com>
2023-12-13 23:14:21 +0100

    big logo first animation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { StartAnimationComponent } from "../start-animation/start-animation.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [StartAnimationComponent]
 })
 export class LoginComponent {
 
