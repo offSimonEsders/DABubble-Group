1df9267bae2e415d28fe5d5bf41db8338e435365
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-14 21:25:10 +0100

    first-create

--- src/app/nav-bar/create-channel/create-channel.component.ts
+++ src/app/nav-bar/create-channel/create-channel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-channel.component.html',
   styleUrl: './create-channel.component.scss'
 })
