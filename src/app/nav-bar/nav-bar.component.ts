908668c9eb3e4bf0642b98451f243d2461f94959
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-14 19:28:16 +0100

    create

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavHeadComponent } from './nav-head/nav-head.component';
 import { ChannelsComponent } from './channels/channels.component';
+import { CreateChannelComponent } from './create-channel/create-channel.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [NavHeadComponent,ChannelsComponent],
+  imports: [
+    NavHeadComponent,
+    ChannelsComponent,
+    CreateChannelComponent,
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
