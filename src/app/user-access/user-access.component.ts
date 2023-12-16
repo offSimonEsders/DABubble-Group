fe6c35b844e2d97c612b0daed86e5878c71fde49
Simon Esders <simonesders2003@gmail.com>
2023-12-16 01:40:36 +0100

    login-header

--- src/app/user-access/user-access.component.ts
+++ src/app/user-access/user-access.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { StartAnimationComponent } from "./start-animation/start-animation.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
     selector: 'app-user-access',
     standalone: true,
     templateUrl: './user-access.component.html',
     styleUrl: './user-access.component.scss',
-    imports: [StartAnimationComponent]
+    imports: [StartAnimationComponent, LoginComponent]
 })
 export class UserAccessComponent {
 
