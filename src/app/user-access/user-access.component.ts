fd81452ec3f883fadcdd68694a74d38c41215201
Simon Esders <simonesders2003@gmail.com>
2023-12-16 23:14:45 +0100

    register component

--- src/app/user-access/user-access.component.ts
+++ src/app/user-access/user-access.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { StartAnimationComponent } from "./start-animation/start-animation.component";
 import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
     selector: 'app-user-access',
     standalone: true,
     templateUrl: './user-access.component.html',
     styleUrl: './user-access.component.scss',
-    imports: [StartAnimationComponent, LoginComponent]
+    imports: [StartAnimationComponent, LoginComponent, RegisterComponent]
 })
 export class UserAccessComponent {
 
