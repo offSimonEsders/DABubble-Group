8f200896cd3fa01831b687608209010bcb0d1ef1
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-20 20:46:09 +0100

    finish compnetnt add User-SCSS

--- src/app/nav-bar/create-channel/add-user/add-user.component.ts
+++ src/app/nav-bar/create-channel/add-user/add-user.component.ts
@@ -9,16 +9,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './add-user.component.scss'
 })
 export class AddUserComponent {
-  choose:boolean = false;
-  openCreate2!:boolean;
-  whichCreate: number = 1;
-
+  choose:boolean = false
   toggleChoose(){
     this.choose = !this.choose;
-  }
-
-  close2(){
-    this.openCreate2 = false;
-    this.whichCreate = 1;
   }
 }
--- src/app/nav-bar/create-channel/create-channel.component.ts
+++ src/app/nav-bar/create-channel/create-channel.component.ts
@@ -16,20 +16,25 @@ import { AddUserComponent } from './add-user/add-user.component';
 export class CreateChannelComponent {
   openCreate: boolean = false;
   whichCreate: number = 1;
+
   @Input() emittedSignal!: any;
-  subscription: any;  
+  subscription: any;
   openCreate2!:boolean;
   constructor(private chatService: ChatService) {}
+
   ngOnInit() {
     this.subscription = this.chatService.channelCreated$.subscribe(
       state => this.openCreate = state
     );
   }
 
-
-
   close() {
     this.openCreate = false;
+  }
+
+  close2(){
+    this.openCreate2 = false;
+    this.whichCreate = 1;
   }
 
   nextpage(){
