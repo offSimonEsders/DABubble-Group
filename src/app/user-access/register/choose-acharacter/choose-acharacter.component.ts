c9f4e13762954a0d712de58e19f5e42ce208923d
Simon Esders <simonesders2003@gmail.com>
2023-12-20 21:56:27 +0100

    switch between windows

--- src/app/user-access/register/choose-acharacter/choose-acharacter.component.ts
+++ src/app/user-access/register/choose-acharacter/choose-acharacter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-choose-acharacter',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './choose-acharacter.component.html',
   styleUrl: './choose-acharacter.component.scss'
 })
-export class ChooseACharacterComponent {
+export class ChooseACharacterComponent implements AfterViewInit {
+  registerframe!: HTMLDivElement;
+  chosecharacterframe!: HTMLDivElement;
+
+  ngAfterViewInit() {
+    this.registerframe = <HTMLDivElement>document.querySelector('.register-frame');
+    this.chosecharacterframe = <HTMLDivElement>document.querySelector('.choose-a-character-frame');
+  }
+
+  showRegister() {
+    this.registerframe.style.display = 'flex';
+    this.chosecharacterframe.style.display = 'none';
+  }
 
 }
