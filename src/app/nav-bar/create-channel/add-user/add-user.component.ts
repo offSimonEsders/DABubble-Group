9847cdef00859080b47d23a32b216ae88c847d15
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-20 20:16:57 +0100

    addUser

--- src/app/nav-bar/create-channel/add-user/add-user.component.ts
+++ src/app/nav-bar/create-channel/add-user/add-user.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.scss'
 })
 export class AddUserComponent {
+  choose:boolean = false;
+  openCreate2!:boolean;
+  whichCreate: number = 1;
 
+  toggleChoose(){
+    this.choose = !this.choose;
+  }
+
+  close2(){
+    this.openCreate2 = false;
+    this.whichCreate = 1;
+  }
 }
--- src/app/nav-bar/create-channel/create-channel.component.ts
+++ src/app/nav-bar/create-channel/create-channel.component.ts
@@ -1,11 +1,12 @@
 import { Component, Injectable, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatService } from '../../services/chat.service';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,AddUserComponent],
   templateUrl: './create-channel.component.html',
   styleUrl: './create-channel.component.scss',
 })
@@ -15,36 +16,25 @@ import { ChatService } from '../../services/chat.service';
 export class CreateChannelComponent {
   openCreate: boolean = false;
   whichCreate: number = 1;
-  choose:boolean = false
   @Input() emittedSignal!: any;
-  subscription: any;
+  subscription: any;  
   openCreate2!:boolean;
   constructor(private chatService: ChatService) {}
-
   ngOnInit() {
-    // Subscribe to changes
     this.subscription = this.chatService.channelCreated$.subscribe(
       state => this.openCreate = state
     );
   }
 
-  toggleChoose(){
-    this.choose = !this.choose;
-  }
+
 
   close() {
     this.openCreate = false;
-  }
-
-  close2(){
-    this.openCreate2 = false;
-    this.whichCreate = 1;
   }
 
   nextpage(){
     this.whichCreate = 2;
 
     this.openCreate2 = true;
-    console.log(this.choose, this.openCreate, this.whichCreate);
   }
 }
