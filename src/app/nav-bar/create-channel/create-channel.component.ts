131ffb80336684790b376f68548201d784fe8298
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-22 11:36:19 +0100

    channel

--- src/app/nav-bar/create-channel/create-channel.component.ts
+++ src/app/nav-bar/create-channel/create-channel.component.ts
@@ -2,11 +2,13 @@ import { Component, Injectable } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatService } from '../../services/chat.service';
 import { AddUserComponent } from './add-user/add-user.component';
+import { FormsModule } from '@angular/forms';
+import { Channel } from '../../models/channel.class';
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
-  imports: [CommonModule,AddUserComponent],
+  imports: [CommonModule,AddUserComponent,FormsModule],
   templateUrl: './create-channel.component.html',
   styleUrl: './create-channel.component.scss',
 })
@@ -16,15 +18,29 @@ import { AddUserComponent } from './add-user/add-user.component';
 export class CreateChannelComponent {
   openCreate: boolean = false;
   whichCreate: number = 1;
-
   subscription: any;
   openCreate2!:boolean;
+  checkDisabled:boolean = true;
+
+  ChannelName:string = '';
+  ChannelDescription:string = '';
+
+  newChannelObject!:Channel;
+
   constructor(private chatService: ChatService) {}
 
   ngOnInit() {
     this.subscription = this.chatService.channelCreated$.subscribe(
       state => this.openCreate = state
     );
+  }
+
+  checkDisabledButton(){
+    if(this.ChannelName.length >= 3){
+      this.checkDisabled = false;
+    }else{
+      this.checkDisabled = true;
+    }
   }
 
   close() {
@@ -40,5 +56,10 @@ export class CreateChannelComponent {
     this.whichCreate = 2;
 
     this.openCreate2 = true;
+  }
+
+  saveNameOfChannel(){
+    let JSON =  new Channel('', [''],this.ChannelName,this.ChannelDescription,false, 'test',)
+    console.log(JSON);
   }
 }
