30c127ceb7effb4d022364d6efebe21d6fcad066
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-12-30 22:00:20 +0100

    log out

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,15 @@ import { AvatarComponent } from '../shared/avatar/avatar.component';
 import { AuthService } from '../services/auth.service';
 import { AccountService } from '../services/account.service';
 import { Account } from '../models/account.class';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  imports: [AvatarComponent],
+  imports: [AvatarComponent,CommonModule],
 })
 export class HeaderComponent implements OnInit {
   authService!: AuthService;
@@ -17,14 +19,23 @@ export class HeaderComponent implements OnInit {
   account!: Account;
   accountStatus!: string;
   accountName!: string;
+  dropDown:boolean = false;
 
-  constructor() {
+  constructor(private router: Router) {
     this.authService = inject(AuthService);
     this.accountService = inject(AccountService);
   }
 
   ngOnInit(): void {
     this.getAccount();
+  }
+
+  switchDropDown(){
+    this.dropDown = !this.dropDown;
+  }
+
+  logOut(){
+    this.router.navigate(['']);
   }
 
   getAccount() {
