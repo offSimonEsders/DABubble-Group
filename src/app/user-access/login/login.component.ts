c114bf9145faba62186de5ea36920bb8342abeae
Simon Esders <simonesders2003@gmail.com>
2023-12-31 12:34:09 +0100

    better login

--- src/app/user-access/login/login.component.ts
+++ src/app/user-access/login/login.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { CheckInputService } from '../../services/check-input.service';
 
 @Component({
   selector: 'app-login',
@@ -8,25 +9,30 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent implements AfterViewInit {
-  sendmailforresetpasswordframe!: HTMLDivElement;
+  sendMailForResetPasswordFrame!: HTMLDivElement;
   loginFrame!: HTMLDivElement;
   registerFrame!: HTMLDivElement;
-  loginInputFrame!: HTMLDivElement;
+  loginInputFrameEmail!: HTMLDivElement;
+  loginuseremailinput!: HTMLInputElement;
+  loginuserpasswordinput! : HTMLInputElement;
 
-  constructor(public authservice: AuthService) {
-
+  constructor(public authService: AuthService, private checkInputService: CheckInputService) {
+    
   }
 
   ngAfterViewInit(): void {
-    this.sendmailforresetpasswordframe = <HTMLDivElement>document.querySelector('.send-mail-for-reset-password-frame');
+    this.sendMailForResetPasswordFrame = <HTMLDivElement>document.querySelector('.send-mail-for-reset-password-frame');
     this.loginFrame = <HTMLDivElement>document.querySelector('.login-frame');
     this.registerFrame = <HTMLDivElement>document.querySelector('.register-frame');
-    this.loginInputFrame = <HTMLDivElement>document.querySelector('.login-input-frame-email');
+    this.loginInputFrameEmail = <HTMLDivElement>document.querySelector('.login-input-frame-email');
+    this.loginuseremailinput = <HTMLInputElement>document.getElementById('login_user_email_input');
+    this.loginuserpasswordinput = <HTMLInputElement>document.getElementById('login_user_password_input');
   }
 
   openSendMailToResetPassword() {
-    this.sendmailforresetpasswordframe.style.display = 'flex';
+    this.sendMailForResetPasswordFrame.style.display = 'flex';
     this.loginFrame.style.display = 'none';
   }
 
@@ -35,13 +41,23 @@ export class LoginComponent implements AfterViewInit {
     this.registerFrame.style.display = 'flex';
   }
 
+  callLoginWithEmailAndPassword() {
+    let emailInput = this.loginuseremailinput.value;
+    let passwordInput = this.loginuserpasswordinput.value;
+    if (this.checkInputService.isValidUseremail(emailInput) && passwordInput.length < 0) {
+      this.authService.authServiceSignInWithEmailAndPassword(emailInput, passwordInput, this.loginFailed);
+      return;
+    }
+    this.loginFailed();
+  }
+
   loginFailed() {
     let loginInputFrame = <HTMLDivElement>document.querySelector('.login-input-frame-email');
     loginInputFrame.classList.add('login-failed');
   }
 
   removeLoginFailed() {
-    this.loginInputFrame.classList.remove('login-failed');
+    this.loginInputFrameEmail.classList.remove('login-failed');
   }
 
 }
