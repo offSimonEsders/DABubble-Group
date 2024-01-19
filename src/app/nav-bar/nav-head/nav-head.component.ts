ee8c6a86a931163d25845d0617e3727de2135aa8
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2024-01-19 15:53:48 +0100

    more resp.

--- src/app/nav-bar/nav-head/nav-head.component.ts
+++ src/app/nav-bar/nav-head/nav-head.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
+import { HomeComponent } from '../../home/home.component';
 
 @Component({
   selector: 'app-nav-head',
@@ -10,14 +11,24 @@ import { ChatService } from '../../services/chat.service';
 })
 export class NavHeadComponent {
   chatService!: ChatService;
+  home!:HomeComponent;
 
   constructor() {
     this.chatService = inject(ChatService);
+    this.home = inject(HomeComponent);
   }
 
   openNewChat() {
     this.chatService.openChatEmitter.next({
       chatColl: '',
     });
+  }
+
+  closeChannelsMobile(){
+    let screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (screenWidth <= 1000) {
+      this.chatService.swichPictureFunction();
+      this.home.swichMobileChat();
+    }
   }
 }
