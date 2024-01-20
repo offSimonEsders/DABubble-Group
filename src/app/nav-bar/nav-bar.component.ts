6a355600631af8d18e2c6c048b97445edda314c5
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2024-01-20 12:12:14 +0100

    add search-bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,11 +3,12 @@ import { NavHeadComponent } from './nav-head/nav-head.component';
 import { ChannelsComponent } from './channels/channels.component';
 import { CreateChannelComponent } from './create-channel/create-channel.component';
 import { ProfileViewComponent } from '../profile-view/profile-view.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [NavHeadComponent, ChannelsComponent, CreateChannelComponent,ProfileViewComponent],
+  imports: [NavHeadComponent, ChannelsComponent, CreateChannelComponent,ProfileViewComponent,SearchBarComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
