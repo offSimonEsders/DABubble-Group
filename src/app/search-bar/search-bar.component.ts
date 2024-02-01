460f7dd49c2594e6e1d8ca766c7473fd92473ff6
RooRar <125753582+RooRar@users.noreply.github.com>
2024-02-02 00:34:39 +0100

    fixed SearchBar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -4,18 +4,20 @@ import { DocumentData, updateDoc } from '@angular/fire/firestore';
 import { MessageService } from '../services/message.service';
 import { ChatService } from '../services/chat.service';
 import { FirestoreService } from '../services/firestore.service';
-import { Channel } from '../models/channel.class';
-import { Chat } from '../models/chat.class';
 import { AccountService } from '../services/account.service';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { ChannelsComponent } from '../nav-bar/channels/channels.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
     selector: 'app-search-bar',
     standalone: true,
     templateUrl: './search-bar.component.html',
     styleUrl: './search-bar.component.scss',
-    imports: [AvatarComponent, CommonModule]
+    providers: [ChannelsComponent],
+    imports: [AvatarComponent, CommonModule,FormsModule]
 })
 export class SearchBarComponent {
   users: any[] = [];
@@ -31,8 +33,9 @@ export class SearchBarComponent {
   firestoreService: FirestoreService;
   accountService!: AccountService;
   authService!: AuthService;
+  input: any;
 
-  constructor() {
+  constructor(public channels: ChannelsComponent) {
     this.messageService = inject(MessageService);
     this.chatService = inject(ChatService);
     this.firestoreService = inject(FirestoreService);
@@ -47,8 +50,7 @@ export class SearchBarComponent {
   }
 
   clearSearch() {
-    let searchInputField = <HTMLInputElement>document.getElementById('searchInputField');
-    searchInputField.value = '';
+    this.input ='';
     this.showResults = false;
   }
 
@@ -75,71 +77,4 @@ export class SearchBarComponent {
 
     this.showResults = (!(this.foundUsers.length + this.foundChannels.length <= 0))
   } 
-
-  openNewChannelDiv() {
-    this.chatService.channelCreated(true);
-  }
-
-  async openChannel(collId: string, channelId: string) {
-    this.clearSearch();
-    this.messageService.checkForExistingMessages(collId, channelId);
-    this.chatService.getChannel(channelId).then((channel: Channel) => {
-      this.chatService.currentChannel = channel;
-      this.chatService.openChatEmitter.next({ chatColl: collId });
-    });
-  }
-
-  // prettier-ignore
-  privateChatExistsFunction(chatColl: string, i: number, accId: string) {
-    this.messageService.checkForExistingMessages(chatColl, this.chatService.chats[i].id);
-    this.chatService.currentChat = this.chatService.chats[i];
-    this.emitChatInfo(chatColl, accId);
-  }
-
-  async openChat(chatColl: string, accId: string) {
-    this.clearSearch();
-    if (this.chatService.chats.length === 0) {
-      this.createNewPrivateChat(chatColl, accId);
-    }
-    for (let i = 0; i < this.chatService.chats.length; i++) {
-      if (this.privateChatExists(i, accId)) {
-        this.privateChatExistsFunction(chatColl, i, accId);
-        break;
-      }
-      if (this.noPrivateChatExists(i)) {
-        this.createNewPrivateChat(chatColl, accId);
-      }
-    }
-  }
-
-  // prettier-ignore
-  privateChatExists(index: number, accId: string) {
-    return (
-      (this.chatService.chats[index].memberIds.includes(accId) &&
-      this.chatService.chats[index].memberIds.includes(this.authService.user.accountId) &&
-      accId !== this.authService.user.accountId) ||
-      (this.chatService.chats[index].memberIds[0] === accId && this.chatService.chats[index].memberIds[1] === accId)
-    );
-  }
-
-  noPrivateChatExists(i: number) {
-    return i === this.chatService.chats.length - 1;
-  }
-
-  async createNewPrivateChat(chatColl: string, accId: string) {
-    let newChat = new Chat('', [this.authService.user.accountId, accId]);
-    this.chatService.addChatOrChannel(newChat, 'chats').then((doc: any) => {
-      updateDoc(doc, { id: doc.id });
-      this.messageService.checkForExistingMessages(chatColl, doc.id);
-      this.chatService.setCurrentChatOrCurrentChannel(chatColl, doc.id);
-    });
-    this.emitChatInfo(chatColl, accId);
-  }
-
-  emitChatInfo(chatColl: string, accId: string) {
-    this.chatService.openChatEmitter.next({
-      chatColl: chatColl,
-      accountId: accId,
-    });
-  }
 }
